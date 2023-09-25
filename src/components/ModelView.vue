c66fd1194b0e0a631f525be4bfe4242857042fdd
Dwiky van Bosstraten <dwikboss99@gmail.com>
2023-09-25 22:22:35 +0200

    added more eyes

--- src/components/ModelView.vue
+++ src/components/ModelView.vue
@@ -3,24 +3,27 @@
         <div id="canvas-wrapper" ref="container"></div>
     </div>
 </template>
-  
+
 <script lang="ts">
-import { defineComponent, onMounted, watchEffect, ref } from 'vue';
 import * as THREE from 'three';
+import { defineComponent, onMounted, watchEffect, ref } from 'vue';
 import { useEditModeStore } from '../store/editMode';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-let totalRotation = 0;
-let rotationSpeed = 0.72;
-let targetRotationSpeed = 0.00;
+// let totalRotation = 0;
+// let rotationSpeed = 0.72;
+// let targetRotationSpeed = 0.00;
 
 let modelYPosition = -1;
 let targetPosition = -0.45;
 
 let modelXRotation = 1.5;
 let targetXRotation = 0;
+
+let modelYRotation = 20;
+let targetYRotation = 0;
 
 let modelScale = 3;
 let targetScale = 3;
@@ -33,7 +36,7 @@ export default defineComponent({
         const container = ref<HTMLElement | null>(null);
         const editModeStore = useEditModeStore();
 
-        let model: THREE.Object3D | null;
+        let bunny: THREE.Object3D | null;
 
         const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 1;
@@ -60,18 +63,27 @@ export default defineComponent({
 
         watchEffect(() => {
             const newState = editModeStore.isEditMode();
-            console.log('Edit mode changed to:', newState);
-
             if (newState === true) {
-                console.log('true');
                 targetScale = 4.5;
                 targetPosition = -0.65;
                 yLerp = 0.11;
             } else {
                 targetScale = 3;
                 targetPosition = -0.45;
+                targetYRotation = 0;
             }
+            // console.log(model ? model.rotation : '');
         });
+
+        function loadEye(model: THREE.Object3D, eyePath: string, position: {x: number, y: number, z: number}, scale: {x: number, y: number, z: number}) {
+            loader.load(eyePath, (eyeGltf) => {
+                const eye = eyeGltf.scene;
+                eye.position.set(position.x, position.y, position.z);
+                eye.scale.set(scale.x, scale.y, scale.z);
+
+                model.add(eye);
+            });
+        }
 
         onMounted(() => {
             if (container.value) {
@@ -79,29 +91,30 @@ export default defineComponent({
             }
 
             loader.load('./src/assets/models/tokki_old.glb', (gltf) => {
-                model = gltf.scene;
-                model.position.set(0, -0.45, 0);
-                model.scale.set(modelScale, modelScale, modelScale);
-                scene.add(model);
+                bunny = gltf.scene;
+                bunny.position.set(0, -0.45, 0);
+                scene.add(bunny);
+
+                loadEye(bunny, './src/assets/models/eyes/star-eye.glb', { x: -0.02, y: 0.168, z: 0.02 }, { x: 1.1, y: 1.1, z: 1.1 });
+                loadEye(bunny, './src/assets/models/eyes/min-eye.glb', { x: 0.02, y: 0.168, z: 0.02 }, { x: 1.1, y: 1.1, z: 1.1 });
             });
             
             const animateModel = () => {
                 requestAnimationFrame(animateModel);
-                if (model) {
-                    rotationSpeed = lerp(rotationSpeed, targetRotationSpeed, 0.036);
-                    model.rotation.y += rotationSpeed;
-                    totalRotation += rotationSpeed;
+                if (bunny) {
+                    modelYRotation = lerp(modelYRotation, targetYRotation, 0.05);
+                    bunny.rotation.y = modelYRotation;
 
                     modelYPosition = lerp(modelYPosition, targetPosition, yLerp);
-                    model.position.y = modelYPosition;
+                    bunny.position.y = modelYPosition;
 
-                    modelXRotation = lerp(modelXRotation, targetXRotation, 0.02);
-                    model.rotation.x = modelXRotation;
+                    modelXRotation = lerp(modelXRotation, targetXRotation, 0.023);
+                    bunny.rotation.x = modelXRotation;
 
                     modelScale = lerp(modelScale, targetScale, 0.12);
-                    model.scale.set(modelScale, modelScale, modelScale);
+                    bunny.scale.set(modelScale, modelScale, modelScale);
 
-                    console.log(modelScale);
+                    console.log(bunny.rotation.y);
                 }
                 renderer.render(scene, camera);
             };
@@ -115,26 +128,6 @@ export default defineComponent({
             controls.enableZoom = false;
             controls.update();
         });
-
-        // let currentScale = initialModelScale;
-        // let currentYPos = initialModelY;
-
-        // const moveModel = () => {
-        //     requestAnimationFrame(moveModel);
-        //     // console.log(this.model.scal);
-        //     let targetScale:number = 5;
-        //     // let targetPosition:number = -5;
-            
-        //     if (model) {
-        //         currentScale = lerp(currentScale, targetScale, 0.1);
-        //         model.scale.set(currentScale, currentScale, currentScale);
-
-        //         // currentYPos = lerp(currentYPos, targetPosition, 0.05);
-        //         // model.position.y = currentYPos;
-        //         model.position.y = 2;
-        //         console.log(model.position.y);
-        //     }
-        // };
 
         const lerp = (a: number, b: number, t: number) => {
             return a + (b - a) * t;
